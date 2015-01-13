72841de53ced74b00d52995ee5a2a1a764b7734e
PandoonQTN <quentin.doucet@outlook.fr>
2015-01-13 10:04:26 +0100

    JS

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,5 @@
+var c,c2, ch1, ch2;
+
 //<!--
 function change_onglet(name) {
     document.getElementById('onglet_' + anc_onglet).className = 'onglet_0 onglet';
@@ -8,8 +10,40 @@ function change_onglet(name) {
 }
 //-->
 
+
+ 
+// ajouter un champ avec son "name" propre;
+function plus(){
+    c=document.getElementById('cadre');
+    c2=c.getElementsByTagName('input');
+    //ch1=document.createElement('input');
+    ch2=document.createElement('input');
+    /*
+    ch1.setAttribute('type','text');
+    ch1.setAttribute('name','ch1'+c2.length);
+    ch1.setAttribute('readonly','readonly'); 
+    ch1.setAttribute('value', 'etiquette'+c2.length/2);
+    ch1.setAttribute('style','border:none');
+    */
+    ch2.setAttribute('type','text');
+    ch2.setAttribute('name','ch'+c2.length);
+    ch2.setAttribute('id','ch'+c2.length);
+    ch2.setAttribute('readonly','readonly'); 
+    //c.appendChild(ch1);
+    c.appendChild(ch2);
+
+    return c2.length;
+}
+
+function moins(i){
+    if(c2.length>0){
+            c.removeChild(c2[i]);
+    }
+    if(c2.length==0){document.getElementById('sup').style.display='none'};
+}
+
 function recupSaisiePopupUser(){
-    
+    var i;
     var tableau = document.getElementById("tabUser");
     
     var ligne = tableau.insertRow(-1);//on a ajouté une ligne
@@ -21,7 +55,13 @@ function recupSaisiePopupUser(){
     
     var nom=document.getElementById("nomUser").value;
     console.log(nom+" est le ");
-    document.getElementById("invisiUser").value=nom;
+    plus();
+    i=(c2.length)-1;
+    console.log('ch'+i);    
+    document.getElementById('ch'+i).value=nom;
+        
+    
+    
 }
 
 function recupSaisiePopupLog(){
@@ -41,6 +81,15 @@ function recupSaisiePopupLog(){
     
     var colonne4 = ligne.insertCell(3);
     colonne4.innerHTML += '<a onclick="supprimerLigneLog(this)">X</a>'; 
+    
+    var nom=document.getElementById("nomUser").value;
+    console.log(nom+" est le ");
+    plus();
+    i=(c2.length)-1;
+    console.log('ch'+i);    
+    document.getElementById('ch'+i).value=nom;
+    
+    
 }
 
 function recupSaisiePopupMaintenance(){
@@ -66,23 +115,33 @@ function recupSaisiePopupMaintenance(){
     
     var colonne2 = ligne.insertCell(5);
     colonne2.innerHTML += '<a onclick="supprimerLigneMaintenance(this)">X</a>';  
+    
+    var nom=document.getElementById("nomUser").value;
+    console.log(nom+" est le ");
+    plus();
+    i=(c2.length)-1;
+    console.log('ch'+i);    
+    document.getElementById('ch'+i).value=nom;
 }
 
 function supprimerLigneMaintenance(r)
 {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById('tabMaintenance').deleteRow(i-1);
+    moins(i-1);
 }
 
 function supprimerLigneUser(r)
 {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById('tabUser').deleteRow(i-1);
+    moins(i-1);
 }
 
 function supprimerLigneLog(r)
 {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById('tabLog').deleteRow(i-1);
+    moins(i-1);
 }
 
