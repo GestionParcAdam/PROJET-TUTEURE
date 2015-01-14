fb20caa653f914d08dad348b9339c657dc1e9376
PandoonQTN <quentin.doucet@outlook.fr>
2015-01-14 09:51:52 +0100

    champs hidden
    pour connaitre le nombre de maintenance

--- src/GestionParcInfo/ParcInfoBundle/Controller/DefaultController.php
+++ src/GestionParcInfo/ParcInfoBundle/Controller/DefaultController.php
@@ -84,6 +84,9 @@ class DefaultController extends Controller
             ->add('revendeur','entity',array('class' => 'ParcInfoBundle:Revendeur', 
                                              'property' => 'nomRevendeur'))
             ->add('immobilisation','text')
+            ->add('nbUsers','hidden')
+            ->add('nbLog','hidden')
+            ->add('nbMaintenance','hidden')
             ->add('nomUser','text')
             ->add('editeur','text')
             ->add('nomLog','text')
