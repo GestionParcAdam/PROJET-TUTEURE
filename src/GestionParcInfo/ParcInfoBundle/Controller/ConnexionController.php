2d29567706f5ce2d8041fbdeb0c5b205cb6cc453
gergrain <akpa.germain@gmail.com>
2015-01-20 09:38:16 +0100

    co et j'ai viré les param qui servent  a rien

--- src/GestionParcInfo/ParcInfoBundle/Controller/ConnexionController.php
+++ src/GestionParcInfo/ParcInfoBundle/Controller/ConnexionController.php
@@ -12,22 +12,44 @@ namespace GestionParcInfo\ParcInfoBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use GestionParcInfo\ParcInfoBundle\Entity\Connexion;
 
 
 class ConnexionController extends Controller
 {
     
-     public function connexionAction(/*Request $request*/)
+     public function connexionAction(Request $request)
     {
+         
          $form = $this->createFormBuilder()
             ->setAction($this->generateUrl('parc_info_connexion'))
             ->add('motDePasse', 'password')
             ->add('connexion', 'submit')
             ->getForm();
-        /* if($form->handleRequest($request)->isSubmitted()){
-             
-         }*/
+        if($form->handleRequest($request)->isSubmitted()){
+            /* j'ouvre la connexion à la BD Doctrine */
+                $em = $this->getDoctrine()->getManager();
+                
+                $data = $form->getData();
+                
+               $co= $em->getRepository('ParcInfoBundle:Connexion')->findOneBy(array('id'=>1));
+               if($co->getMdp()==sha1($data['motDePasse'])){
+                   $materiels = $em->getRepository('ParcInfoBundle:Materiel')   
+                       ->getMaterielsHS();
+        $allsite = $em->getRepository('ParcInfoBundle:Site')->findAll();
+        
+        $materielPG = $em->getRepository('ParcInfoBundle:Materiel')->getMaterielsPG();
+        
+        $materielEnPanne = $em->getRepository('ParcInfoBundle:Materiel')->getMaterielEnPanne();
+        
+        $dernierModif = $em->getRepository('ParcInfoBundle:Materiel')->getDernierMateriels();
+        
+       
+        return $this->render('ParcInfoBundle:Default:index.html.twig', 
+                                array('materielHs' => $materiels,'allsite' => $allsite,'materielPG'=>$materielPG,'materielEnPanne'=>$materielEnPanne,'dernierModif'=>$dernierModif));
+               }
+        }
         return $this->render('ParcInfoBundle:Default:Connexion/Connexion.html.twig', array('form' => $form->createView()));
     }
  }
